e8b02152a81353cda528bd7750caf2a9510217c5
kingabreel <gabrielyury441@gmail.com>
2024-06-21 11:00:24 -0300

    first commit

---
+++ src/main/java/one/alura/forumhub_one/ForumHubOneApplication.java
@@ -0,0 +1,13 @@
+package one.alura.forumhub_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumHubOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumHubOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/alura/forumhub_one/ForumHubOneApplicationTests.java
@@ -0,0 +1,13 @@
+package one.alura.forumhub_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
